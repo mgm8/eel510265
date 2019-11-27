a89753622d9fd4c8e3ec246309ce967e6bbf25d2
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2019-11-26 23:55:28 -0300

    projeto_final: firmware: Updating the main function

--- projeto_final/firmware/src/main.cpp
+++ projeto_final/firmware/src/main.cpp
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.4.6
+ * \version 0.5.0
  * 
  * \date 20/10/2019
  * 
@@ -38,16 +38,34 @@
 #include <tasks/startup.h>
 #include <tasks/time_control.h>
 #include <tasks/save_log.h>
+#include <tasks/read_option.h>
+#include <tasks/wait_coins.h>
+
+#include <vending_machine.h>
 
 using namespace vmos;
 
 int main(int argc, char *argv[])
 {
+    Task *startup = new TaskStartup();
+    Task *time_control = new TaskTimeControl();
+    Task *save_log = new TaskSaveLog();
+    Task *read_option = new TaskReadOption();
+    Task *wait_coins = new TaskWaitCoins();
+
+    vending_machine.tasks.push_back(startup);
+    vending_machine.tasks.push_back(time_control);
+    vending_machine.tasks.push_back(save_log);
+    vending_machine.tasks.push_back(read_option);
+    vending_machine.tasks.push_back(wait_coins );
+
     Scheduler vm_scheduler;
 
-    vm_scheduler.add_task(new TaskStartup());
-    vm_scheduler.add_task(new TaskTimeControl());
-    vm_scheduler.add_task(new TaskSaveLog());
+    vm_scheduler.add_task(startup);
+    vm_scheduler.add_task(time_control);
+    vm_scheduler.add_task(save_log);
+    vm_scheduler.add_task(read_option);
+    vm_scheduler.add_task(wait_coins );
 
     vm_scheduler.run();
 
